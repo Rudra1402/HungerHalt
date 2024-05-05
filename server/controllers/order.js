8ef7238832b4a3457ee3e6c9e9f0007ff8225b64
Rudra Patel <rudra.190410116095@gmail.com>
2024-05-04 22:52:32 -0400

    order id verification

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -1,9 +1,25 @@
+const User = require('../models/user');
 const Order = require('../models/order');
 const Partner = require('../models/partner');
+const nodemailer = require('nodemailer')
+
+let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    host: 'smtp.gmail.com',
+    auth: {
+        user: 'bankingapp.ba5@gmail.com',
+        pass: process.env.EMAIL_PASS
+    }
+});
 
 exports.createOrder = async (req, res) => {
     const orderData = req.body;
     try {
+        const user = await User.findById(req.body.userId)
+        if (!user) {
+            return res.status(404).json({ message: "User not found!" });
+        }
+
         const newOrder = new Order(orderData);
 
         if (newOrder.isOrderIDVerified && newOrder.paymentStatus) {
@@ -11,6 +27,30 @@ exports.createOrder = async (req, res) => {
         }
 
         await newOrder.save();
+
+        const emailHTML = `
+            <div style="padding: 8px; background-color: lightgreen; border-radius: 5px;">
+                <h1 style="color: blue;">Order Confirmation</h1>
+                <p style="color: blue;">Thank you for your order with HungerHalt. Your order ID is: ${req.body.uniqueOrderId}</p>
+                <p>This is an automated email, please do not reply.</p>
+            </div>
+        `;
+
+        let mailOptions = {
+            from: 'bankingapp.ba5@gmail.com',
+            to: user.email,
+            subject: 'Order Confirmation',
+            html: emailHTML
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            res.status(201).json({ message: 'Email sent successfully!' });
+        });
+
         res.status(201).json({ message: "Order placed successfully!" });
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -49,7 +89,7 @@ exports.getOrdersByPartner = async (req, res) => {
                 path: 'userId',
                 model: 'User',
                 select: 'name'
-            })
+            }).sort({ createdAt: -1 })
 
         if (!orders) {
             return res.status(404).json({ message: 'No orders found!' })
@@ -63,31 +103,22 @@ exports.getOrdersByPartner = async (req, res) => {
 
 exports.updateOrder = async (req, res) => {
     const orderId = req.params.orderId;
-    const updates = req.body;
+    const partnerId = req.body.partnerId;
     try {
-        const order = await Order.findById(orderId);
+        const order = await Order.findOne({ uniqueOrderId: orderId });
         if (!order) {
-            return res.status(404).json({ error: "Order not found!" });
+            return res.status(404).json({ message: "Order not found!" });
         }
 
-        if (order.isOrderIDVerified && order.paymentStatus) {
-            await Partner.updateOne({ _id: order.partnerId }, { $inc: { ordersPlaced: 1 } });
-        }
+        order.isOrderIDVerified = true;
+        order.paymentStatus = true;
 
-        for (const key in updates) {
-            if (key !== 'postId') {
-                order[key] = updates[key];
-            }
-        }
-
-        if (updates.postId) {
-            order.postId.push(updates.postId);
-        }
+        await Partner.updateOne({ _id: partnerId }, { $inc: { ordersPlaced: 1 } });
 
         const updatedOrder = await order.save();
 
-        return res.status(200).json({ message: "Order updated successfully!", updatedOrder });
+        return res.status(200).json({ message: "Order updated successfully!" });
     } catch (error) {
-        res.status(500).json({ error: "Error: " + error.message });
+        res.status(500).json({ message: "Error: " + error.message });
     }
 };
